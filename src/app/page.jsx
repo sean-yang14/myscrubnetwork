9e9c540b679e256b8d39f7495316d5b5fe5d6cf5
Sean Y <seanhyang1@gmail.com>
2023-03-19 18:53:14 -0400

    trying of fix home page redirect

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,20 @@
 'use client';
 
+// import { useRouter } from 'next/navigation';
+
+// export default function Home() {
+// 	const router = useRouter();
+// 	router.push('/jobs');
+// }
+
+import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
 	const router = useRouter();
-	router.push('/jobs');
+
+	useEffect(() => {
+		router.push('/jobs');
+	}, []);
 }
-``;
